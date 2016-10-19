af2f62229f5a9146efa8c73ab173bd405999d057
Kateryna Leontieva <katerynaleontieva@Katerynas-MacBook-Pro.local>
2016-10-19 08:56:19 +0300

    linked listimpl

---
+++ LinkedListImpl/ConsoleApplication1/MyLinkedList.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class MyLinkedList : IEnumerable
+    {
+        private ListNode head;
+        private ListNode tail;
+        private int size;
+
+        class MyIENumerator : IEnumerator
+        {
+            ListNode position;
+            ListNode head;
+            public MyIENumerator(ListNode position)
+            {
+                this.position = position.prev;
+                head = position;
+            }
+            public bool MoveNext()
+            {
+                if(position == null)
+                {
+                    position = head;
+                    return true;   
+                }
+                position = position.next;
+                return position != null;
+            }
+
+            public void Reset()
+            {
+                position = head;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    return position.item;
+                }
+            }
+        }
+
+        public class ListNode
+        {
+            public object item;
+            public ListNode next;
+            public ListNode prev;
+
+            public ListNode(object item, ListNode next, ListNode prev)
+            {
+                this.item = item;
+                this.next = next;
+                this.prev = prev;
+            }
+
+            public ListNode()
+            {
+            }
+        }
+
+        public MyLinkedList()
+        {
+            size = 0;
+            head = null;
+        }
+
+        public int Length()
+        {
+            return size;
+        }
+        public void Add(object item)
+        {
+            if (size == 0)
+            {
+                ListNode l1 = new ListNode();
+                l1.item = item;
+                l1.next = l1.prev = null;
+                head = tail = l1;
+                size++;
+            }
+            else
+            {
+                ListNode ln = new ListNode();
+                ln.item = item;
+                ln.next = null;
+                ln.prev = tail;
+                tail.next = ln;
+                tail = ln;
+                size++;
+            }
+        }
+
+        public void AddAt(object item, int index)
+        {
+            if (index < 0 || index > size - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (size == 0)
+            {
+                Add(item);
+            }
+            else
+            {
+                ListNode ln = new ListNode();
+                ListNode currentNode = GetListNodeAt(index);
+                ln.item = item;
+                ln.prev = currentNode.prev;
+                ln.next = currentNode;
+                if (currentNode == head)
+                {
+                    head = ln;
+                    currentNode.prev = ln;
+                }
+                else
+                {
+                    currentNode.prev.next = ln;
+                    currentNode.prev = ln;
+
+                }
+                size++;
+            }
+        }
+
+        public object EllementAt(int index)
+        {
+            return GetListNodeAt(index).item;
+
+        }
+
+        public void Remove()
+        {
+            if (size == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            tail = tail.prev;
+            if (tail != null)
+            {
+                tail.next = null;
+            }
+            size--;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > size - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (index == size + 1)
+            {
+                Remove();
+            }
+            if (index == 0)
+            {
+                ListNode ln = GetListNodeAt(index);
+                ln.next.prev = null;
+                head = ln.next;
+                ln = null;
+                size--;
+            }
+            else
+            {
+                ListNode ln = GetListNodeAt(index);
+                ln.next.prev = ln.prev;
+                ln.prev.next = ln.next;
+                size--;
+            }
+        }
+        private ListNode GetListNodeAt(int index)
+        {
+            ListNode currentNode = head;
+            for (int i = 0; i < index; i++)
+            {
+                currentNode = currentNode.next;
+            }
+            return currentNode;
+        }
+        
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new MyIENumerator(head);
+        }
+
+    }
+
+   
+}
---
+++ LinkedListImpl/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestLengthAndAdd()
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.Add(12);
+            list.Add("string");
+            list.Add(true);
+
+            Assert.AreEqual(3, list.Length());
+        }
+
+        [TestMethod]
+        public void TestAddAtFirstPosition()
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.Add(12);
+            list.Add(11);
+            list.Add(10);
+            list.AddAt(4, 0);
+            Assert.AreEqual(4, list.EllementAt(0));
+        }
+
+        [TestMethod]
+        public void TestAddAtLastPosition()
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.Add(12);
+            list.Add(11);
+            list.Add(10);
+            list.AddAt(4, 2);
+
+            Assert.AreEqual(4, list.EllementAt(2));
+        }
+        [TestMethod]
+        public void TestAddAtTheMiddle()
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.Add(12);
+            list.Add(11);
+            list.Add(10);
+            list.AddAt(4, 1);
+            Assert.AreEqual(4, list.EllementAt(1));
+        }
+
+        [TestMethod]
+        public void TestRemoveFromTheEnd()
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.Add(12);
+            list.Add(11);
+            list.Add(10);
+            list.Remove();
+    
+            Assert.AreEqual(2, list.Length());
+            Assert.AreEqual(11, list.EllementAt(1));
+        }
+
+        [TestMethod]
+        public void TestRemoveFromTheEndIfOnlyOneElementPresent()
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.Add(12);
+            list.Remove();
+
+            Assert.AreEqual(0, list.Length());
+        }
+
+        [TestMethod]
+        public void TestRemoveFromTheBeginning()
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.Add(12);
+            list.Add(11);
+            list.Add(10);
+            list.Add(9);
+            list.Add(8);
+            list.RemoveAt(0);
+            Assert.AreEqual(4, list.Length());
+            Assert.AreEqual(11, list.EllementAt(0));
+        }
+
+        [TestMethod]
+        public void TestForEach()
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.Add(12);
+            list.Add(11);
+            list.Add(10);
+            list.Add(9);
+            foreach(int item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        [TestMethod]
+        public void TestRemoveFromTheMiddle()
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.Add(12);
+            list.Add(11);
+            list.Add(10);
+            list.Add(9);
+            list.RemoveAt(2);
+            Assert.AreEqual(3, list.Length());
+            Assert.AreEqual(11, list.EllementAt(1));
+            Assert.AreEqual(9, list.EllementAt(2));
+        }
+    }
+}
