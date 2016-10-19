1ab8dfd75c36e5290b6849eeec865072d6781f16
Kateryna Leontieva <katerynaleontieva@Katerynas-MacBook-Pro.local>
2016-10-19 08:53:01 +0300

    f

---
+++ HashTableImpl/HashTableImpl/MyHashTable.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableImpl
+{
+    public class MyHashTable : IHashTable
+    {
+        private static readonly int initialCapacity = 100;
+
+        private int capacity = initialCapacity;
+        private HashTableNode[] hashTable;
+
+        public MyHashTable(int capacity)
+        {
+            hashTable = new HashTableNode[capacity];
+        }
+
+        public MyHashTable()
+        {
+            hashTable = new HashTableNode[capacity];
+        }
+
+        public class HashTableNode
+        {
+            public int hash;
+            public object key;
+            public object value;
+            public HashTableNode next;
+        }
+
+        private int Hash(object key)
+        {
+            return Math.Abs(key.GetHashCode() % capacity);
+        }
+
+        public object this[object key]
+        {
+            get
+            {
+                return FindEllement(key).value;
+            }
+
+            set
+            {
+                FindEllement(key).value = value;
+            }
+        }
+
+        public bool Contains(object key)
+        {
+            int hashOfKey = Hash(key);
+            if (hashTable[hashOfKey] == null)
+            {
+                return false;
+            }
+            HashTableNode current = hashTable[hashOfKey];
+            do
+            {
+                if (current.key.Equals(key))
+                {
+                    return true;
+                }
+                current = current.next;
+            } while (current != null);
+
+            return false;
+        }
+
+        public void Add(object key, object value)
+        {
+            int hashOfKey = Hash(key);
+            if (Contains(key))
+            {
+                throw new SystemException();
+            }
+            HashTableNode newNode = new HashTableNode();
+            newNode.hash = hashOfKey;
+            newNode.key = key;
+            newNode.value = value;
+            HashTableNode current = hashTable[hashOfKey];
+            if (current == null)
+            {
+                hashTable[hashOfKey] = newNode;
+            }
+            else
+            {
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+        }
+        public bool TryGet(object key, out object value)
+        {
+            if (!Contains(key))
+            {
+                value = null;
+                return false;
+            }
+            HashTableNode current = FindEllement(key);
+            value = current.value;
+            return true;
+        }
+
+        private HashTableNode FindEllement(object key)
+        {
+            if (!Contains(key))
+            {
+                throw new SystemException();
+            }
+            int hashOfKey = Hash(key);
+            HashTableNode current = hashTable[hashOfKey];
+            do
+            {
+                if (current.key.Equals(key))
+                {
+                    return current;
+                }
+                current = current.next;
+            } while (current != null);
+
+            return current;
+        }
+
+    }
+}
---
+++ HashTableImpl/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HashTableImpl;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestContainsWhenOneEllement()
+        {
+            MyHashTable hashTable = new MyHashTable();
+            hashTable.Add("kate", "lysenko");
+            Assert.IsTrue(hashTable.Contains("kate"));
+        }
+
+        [TestMethod]
+        public void TestContainsWhenSeveralEllements()
+        {
+            MyHashTable hashTable = new MyHashTable();
+            hashTable.Add("kate", "lysenko");
+            hashTable.Add("maryna", "zlotko");
+            hashTable.Add(1, true);
+            Assert.IsTrue(hashTable.Contains("kate"));
+        }
+
+        [TestMethod]
+        public void TestContainsInTheMiddleWhenSeveralEllements()
+        {
+            MyHashTable hashTable = new MyHashTable();
+            hashTable.Add("kate", "lysenko");
+            hashTable.Add("maryna", "zlotko");
+            hashTable.Add(1, true);
+            Assert.IsTrue(hashTable.Contains("maryna"));
+        }
+
+        [TestMethod]
+        public void TestContainsLastWhenSeveralEllements()
+        {
+            MyHashTable hashTable = new MyHashTable();
+            hashTable.Add("kate", "lysenko");
+            hashTable.Add("maryna", "zlotko");
+            hashTable.Add(1, true);
+            Assert.IsTrue(hashTable.Contains(1));
+        }
+
+        [TestMethod]
+        public void TestContainsIfNotPresent()
+        {
+            MyHashTable hashTable = new MyHashTable();
+            hashTable.Add("kate", "lysenko");
+            hashTable.Add("maryna", "zlotko");
+            hashTable.Add(1, true);
+            Assert.IsFalse(hashTable.Contains("KeyNotPresent"));
+        }
+
+        [TestMethod]
+        public void TestTryGet()
+        {
+            MyHashTable hashTable = new MyHashTable();
+            hashTable.Add("kate", "lysenko");
+            hashTable.Add("maryna", "zlotko");
+            hashTable.Add(1, true);
+            object testValue;
+            hashTable.TryGet("kate", out testValue);
+            Assert.AreEqual("lysenko", testValue);
+        }
+
+        [TestMethod]
+        public void TestTryGetIfNotPresent()
+        {
+            MyHashTable hashTable = new MyHashTable();
+            hashTable.Add("kate", "lysenko");
+            hashTable.Add("maryna", "zlotko");
+            hashTable.Add(1, true);
+            object testValue;
+            hashTable.TryGet("notPresent", out testValue);
+            Assert.IsNull(testValue);
+        }
+
+        [TestMethod]
+        public void TestGet()
+        {
+            MyHashTable hashTable = new MyHashTable();
+            hashTable.Add("kate", "lysenko");
+            hashTable.Add("maryna", "zlotko");
+            hashTable.Add(1, true);
+            Assert.AreEqual("zlotko", hashTable["maryna"]);
+        }
+
+        [TestMethod]
+        public void TestSet()
+        {
+            MyHashTable hashTable = new MyHashTable();
+            hashTable.Add("kate", "lysenko");
+            hashTable.Add("maryna", "zlotko");
+            hashTable.Add(1, true);
+            hashTable["maryna"] = "gybelynda";
+            Assert.AreEqual("gybelynda", hashTable["maryna"]);
+        }
+    }
+}
